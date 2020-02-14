137a67f49b74ad3369826147c7e1e2568ae2c414
1903larissaalves <1903larissaalves@gmail.com>
2020-02-13 22:22:59 -0300

    Ajustes no calculo

--- calculadora.js
+++ calculadora.js
@@ -1,31 +1,46 @@
 
-function valores(valor) {
-    
+function obterTeclaDigitada(valor) {
+
     document.getElementById("resultado").value += valor;
 }
 
-function calculo() {
+function calcularResultado() {
     let digitado = document.getElementById("resultado").value;
-    
-    let posicaoSimbolo = digitado.indexOf('%');
-    if(posicaoSimbolo >= 0){
-        porcento();
-    }else{
-        let converteNumero = eval(digitado);
-        document.getElementById("resultado").value = converteNumero;
+
+    if (operacaoPorcentagem(digitado)) {
+        calculoPorcentagem(digitado);
+    } else {
+        calculoNormal(digitado);
     }
 }
 
-function apaga() {
+function operacaoPorcentagem(digitado) {
+    let posicaoSimbolo = digitado.indexOf('%');
+    if (posicaoSimbolo >= 0) {
+        return true;
+    } else {
+        return false;
+    }
+    // return (posicaoSimbolo >= 0); 
+}
+
+function calculoNormal(digitado) {
+    let resultadoCalculo = eval(digitado);
+    if (resultadoCalculo == Infinity) {
+        document.getElementById("resultado").value = "Não é possível fazer divisão por 0"
+    } else {
+        document.getElementById("resultado").value = resultadoCalculo;
+    }
+}
+
+function limparCampo() {
     document.getElementById("resultado").value = "";
 }
 
-function porcento() {
-    let digitado = document.getElementById("resultado").value;
-
+function encontrarPosicaoSimbolo(digitado) {
     let posicaoSimbolo = digitado.indexOf('-');
 
-    if(posicaoSimbolo < 0){
+    if (posicaoSimbolo < 0) {
         posicaoSimbolo = digitado.indexOf('+');
     }
     if (posicaoSimbolo < 0) {
@@ -34,18 +49,38 @@ function porcento() {
     if (posicaoSimbolo < 0) {
         posicaoSimbolo = digitado.indexOf('*');
     }
+    return posicaoSimbolo;
+}
+
+
+function calculoPorcentagem(digitado) {
+
+    let posicaoSimbolo = encontrarPosicaoSimbolo(digitado);
 
     let valorAntesSinal = digitado.substring(0, posicaoSimbolo);
-    let porcento = digitado.length -1   ;
-    let valorDepoisSinal = digitado.substring(posicaoSimbolo +1, porcento);
-    let resultado = eval((valorDepoisSinal * 100)/ valorAntesSinal);
-    let resultadoFinal = resultado / 100;
-    let result = eval(valorAntesSinal) + eval(digitado.substring(posicaoSimbolo, 1)) + eval(resultadoFinal);
+    let posicaoPorcento = digitado.length - 1;
+    let valorDepoisSinal = (digitado.substring(posicaoSimbolo + 1, posicaoPorcento));
+    let multi = valorAntesSinal * valorDepoisSinal;
+    let divisao = multi / 100;
+
+    obterResultado(posicaoSimbolo, digitado, valorAntesSinal, divisao)
+}
+
+
+function obterResultado(posicaoSimbolo, digitado, valorAntesSinal, divisao) {
+
+    let result;
+    if (posicaoSimbolo == digitado.indexOf('+')) {
+        result = eval(valorAntesSinal) + eval(divisao);
+    }
+    if (posicaoSimbolo == digitado.indexOf('-')) {
+        result = eval(valorAntesSinal) - eval(divisao);
+    }
 
     document.getElementById("resultado").value = result;
 }
 
-function mensagemEscondida(){
+function mensagemEscondida() {
     console.log("Não tem nada ai");
 }
 
